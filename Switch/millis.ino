4c464b35eb5f0e0ec74320c033232c8e586e9e60
Sir Albert <31673212+TheCrazyBaconLord@users.noreply.github.com>
2017-11-22 09:57:27 -0500

    Create millis.ino

---
+++ Switch/millis.ino
@@ -0,0 +1,37 @@
+//millis()
+
+   /* SimpleToggle_PB_LED */
+
+const byte LEDpin = 3;
+const byte SWITCHpin = 2;
+
+byte LEDstate = HIGH;
+byte SWITCHstate;
+byte lastSWITCHstate;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LEDpin, OUTPUT);
+pinMode(SWITCHpin, INPUT);
+digitalWrite(SWITCHpin, HIGH);
+}
+
+unsigned long lastTime = millis();
+const long interval = 200;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+SWITCHstate = digitalRead(SWITCHpin);
+if ((lastSWITCHstate == HIGH) && (SWITCHstate == LOW) && (millis() - lastTime >= interval))
+{
+    
+    LEDstate = !LEDstate;
+    digitalWrite(LEDpin, LEDstate);
+    //delay(millis() - lastTime);
+    
+    lastTime = millis();
+}
+  
+  lastSWITCHstate = SWITCHstate;
+}
